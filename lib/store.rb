9222825c73ef937787dd59e992fa2dbcd93128ba
Bryce <brycewalterparker@gmail.com>
2017-10-10 17:32:03 +0000

    completed exercise 9

--- lib/store.rb
+++ lib/store.rb
@@ -3,10 +3,20 @@ class Store < ActiveRecord::Base
   validates :name, length: { minimum: 3 }
   validates :annual_revenue, numericality: { greater_than: 0 }
   validate :must_carry
-end
 
-def must_carry
-  if :mens_apparel === false && :womens_apparel === false
-    errors.add(:mens_apparel, :womens_apparel, "Must carry clothes")
+  before_destroy :is_empty?
+
+private
+
+  def must_carry
+    if :mens_apparel === false && :womens_apparel === false
+      errors.add(:mens_apparel, :womens_apparel, "Must carry clothes")
+    end
+  end
+
+  def is_empty?
+    if self.employees.size > 0
+      return false
+    end
   end
 end
