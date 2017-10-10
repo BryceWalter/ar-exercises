ce70ed24632a987767c21f290f8eb09c1de960de
Bryce <brycewalterparker@gmail.com>
2017-10-10 16:08:10 +0000

    completed exercise 7 with bonus

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,14 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
+puts "Please enter a new store name:"
+user_store_name = gets.chomp
+
+@user_store = Store.new(name: user_store_name)
+@user_store.valid?
+
+@user_store.errors.messages.each do |key, value|
+  puts "Error: #{value}"
+end
+
 # Your code goes here ...
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,12 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than: 0 }
+  validate :must_carry
+end
+
+def must_carry
+  if :mens_apparel === false && :womens_apparel === false
+    errors.add(:mens_apparel, :womens_apparel, "Must carry clothes")
+  end
 end
